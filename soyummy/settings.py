1251ab7867c04e72c51810e0487d3c4d34c91e0e
annamiklas <78590946+annamiklas@users.noreply.github.com>
2021-05-25 11:23:57 +0200

    Update settings.py

--- soyummy/settings.py
+++ soyummy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y@l&j81mwnkwi-m_xl*e*xhcsy51-4f+%dpj&q-5lrp&8kj46='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'cookbook',
     'category',
     'widget_tweaks',
-    'sorter',
 ]
 
 MIDDLEWARE = [
@@ -140,8 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = [
-    '192.168.0.170',
-    '172.20.10.2',
+    '127.0.0.1',
 ]
 
 # Email veryfication sender
