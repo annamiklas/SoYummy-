8c7240c55856d8aeb455e5a47d696601bb5421bb
Ania <annamiklas7@gmail.com>
2021-05-26 14:15:45 +0200

    Update settings.py

--- soyummy/settings.py
+++ soyummy/settings.py
@@ -145,6 +145,6 @@ ALLOWED_HOSTS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'soyummy.cookapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'soyummy2021'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
